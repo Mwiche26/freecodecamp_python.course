f7ecb35888e7f4344a51e27ac534c09297885d56
Mwiche Mwiche <mwichebusiness26@gmail.com>
2025-05-02 17:38:44 +0200

    2nd commit

--- if statements.py
+++ if statements.py
@@ -1,2 +1,23 @@
-is_male = true
-if 
+is_male = False
+is_tall = True
+if is_male or is_tall:
+    print("you are a male or tall or both")
+elif is_male and not(is_tall):
+    print("you are short male")
+elif not ("is_male") and is_tall:
+    print("you are not male but tall")
+else:
+    print("you are a male neither male nor tall")
+
+#comparisons inside if statements
+def max_num( num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+print(max_num(3,4,5))
+
+
